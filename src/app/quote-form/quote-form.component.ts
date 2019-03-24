5442f982f661215e6ed0ae34deeea219e4893fcf
System Administrator <root@Owners-MacBook-Pro.local>
2019-03-24 21:07:36 +0300

    update form component class

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote-form',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
-
+  newQuote=new Quote(0,"","",new Date());
+  @Output() addQuote=new EventEmitter<Quote>();
+    
+  submitQuote(){
+      this.addQuote.emit(this.newQuote);
+  }
   constructor() { }
 
   ngOnInit() {
   }
 
 }
+
