a8076954f5ac2a7120ca453186824ac540bc69ee
System Administrator <root@Owners-MacBook-Pro.local>
2019-03-24 20:57:21 +0300

    add dislike functionalityt

--- src/app/like-dislike/like-dislike.component.ts
+++ src/app/like-dislike/like-dislike.component.ts
@@ -6,7 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./like-dislike.component.css']
 })
 export class LikeDislikeComponent implements OnInit {
+  likesNumber : number = 0;
+  dislikesNumber: number = 0;
 
+  likeButtonClick(){
+    this.likesNumber++;
+  }
+  dislikeButtonClick(){
+    this.dislikesNumber++;
+  }
   constructor() { }
 
   ngOnInit() {
